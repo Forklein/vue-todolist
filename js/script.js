ee909cac5e893e9330721ebf6a9dad3a95a31b20
Forklein <giuseppepisani2@gmail.com>
2021-07-29 14:39:39 +0200

    Set v-for

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-console.log('Vue is activated,' Vue);
-
+console.log('Vue is activated', Vue);
 Vue.config.devtools = true;
+
+
+const root = new Vue({
+    el: "#app",
+    data: {
+        list: [
+            "Seguire Boolean",
+            "Fare la spesa",
+            "Uscire",
+            "Dormire",
+        ],
+    },
+    methods: {},
+});
+
+
+
